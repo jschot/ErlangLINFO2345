d20e35d4a666a1a3b61f05bf9cf84b36b1b90d69
jschot <jean.schot@student.uclouvain.be>
2022-12-03 13:50:15 +0100

    fix globalvar + incr ages

--- node.erl
+++ node.erl
@@ -1,37 +1,48 @@
 -module(node).
--export([start/3, createnode/1, init/1, rps/3]).
+-export([start/3, start/1, createnode/1, init/1, rps/3]).
 
 start(N, L, V) ->
+    ets:new(x, [set,public,named_table]),
+    ets:insert(x, {n, N}),
+    ets:insert(x, {l, L}),
+    ets:insert(x, {v, V}),
+    start(N).
+
+start(N) ->
     C = 0,
     if
         C == N ->
-            io:fwrite("All node created~n"),
-            Globalvar = ets:new(x, []),
-            ets:insert(Globalvar, {l, L}),
-            ets:insert(Globalvar, {v, V});
+            io:fwrite("All node created~n");
         true ->
             createnode(list_to_atom(integer_to_list(N))),
-            start(N-1,L)
+            start(N-1)
     end.
 
 createnode(ID) ->
     register(ID, spawn(node, init, [ID])).
 
 init(ID) ->
-    rps(ID,[],[]).
+    N1 = ets:lookup(x, n),
+    V1 = ets:lookup(x, v),
+    [{_, N}] = N1,
+    [{_, V}] = V1,
+
+    Table = [list_to_atom(integer_to_list(rand:uniform(N))) || _ <- lists:seq(1, V)],
+    Ages = [0 || _ <- lists:seq(1, V)],
+    rps(ID,Table,Ages).
 
 rps(ID, Table, Ages) ->
-    Ages = [X+1||{_,X} <- Ages],
+    AgesInc = [X+1||X <- Ages],
     receive
         {request, From, Entries} ->
             io:format("received ~w~n ", [Entries]),
             NewTable = Table ++ From,
-            NewAges = Ages ++ [0],
+            NewAges = AgesInc ++ [0],
             rps(ID, NewTable, NewAges);
         showtable ->
             io:format("Table ~w~n", [Table]),
-            io:format("Age ~w~n", [Ages]),
-            rps(ID, Table, Ages);
+            io:format("Age ~w~n", [AgesInc]),
+            rps(ID, Table, AgesInc);
         stop ->
             io:format("closing down~n", []),
             ok
